24af0c2d1c35e889855bc65d460da231e89a3e0d
GarnierValentin <valentin.garnier@oclock.school>
2024-06-25 19:46:44 +0200

    change for https

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,14 @@
 import 'dotenv/config';
 import app from '../app/index.app.js';
 import { createServer } from 'node:http';
-const server = createServer(app);
-const { PORT } = process.env;
+// import { createServer } from 'node:https';
+import fs from 'node:fs';
+const httpsOptions = {
+    key: fs.readFileSync(process.env.KEY),
+    cert: fs.readFileSync(process.env.CRT),
+};
+const { PORT, KEY, CRT } = process.env;
+const server = createServer(/* httpsOptions, */ app);
 server.listen(PORT, () => {
     console.log(`Express is listening at http://localhost:${PORT}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config';
 import app from '../app/index.app.js';
 import { createServer } from 'node:http';
+// import { createServer } from 'node:https';
+import fs from 'node:fs';
 
-const server = createServer(app);
+const httpsOptions = {
+  key: fs.readFileSync(process.env.KEY),
+  cert: fs.readFileSync(process.env.CRT),
+};
 
-const { PORT } = process.env;
+const { PORT, KEY, CRT } = process.env;
+
+const server = createServer(/* httpsOptions, */app);
 
 server.listen(PORT, () => {
   console.log(`Express is listening at http://localhost:${PORT}`);
